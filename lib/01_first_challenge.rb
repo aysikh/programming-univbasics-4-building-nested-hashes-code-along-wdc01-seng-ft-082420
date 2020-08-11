fcb793d00a985f0e5009388be358f11a8be2f657
Amy Sikhammountry <amy.sikhammountry@gmail.com>
2020-08-11 16:31:17 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,3 +1,16 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     :montague => {
+       :patriarch => {}, 
+       :matriarch => {}, 
+       :heroine => {}, 
+       :heroine_friends => []
+     }
+     :capulet => {
+       :patriarch => {}, 
+       :matriarch => {}, 
+       :heroine => {}, 
+       :heroine_friends => [] #remember this is in a array because there are multiple variables being inserted here
+     }
+   }
 end
