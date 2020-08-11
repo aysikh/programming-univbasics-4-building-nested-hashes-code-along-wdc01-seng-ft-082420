225a5d31f60636f9aae26b93b9d0b0fb804ebcec
Amy Sikhammountry <amy.sikhammountry@gmail.com>
2020-08-11 16:38:37 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,16 +1,54 @@
 def first_challenge
    epic_tragedy = {
      :montague => {
-       :patriarch => {}, 
-       :matriarch => {}, 
-       :heroine => {}, 
-       :heroine_friends => []
+       :patriarch => {
+         :name => "Lord Montague",
+         :age => "53"
+       }, 
+       :matriarch => {
+         :name => "Lady Montague",
+         :age => "54"
+       }, 
+       :heroine => {
+         :name => "Romeo", 
+         :age = "15",
+         :status = "Alive"
+       }, 
+       :heroine_friends => [
+         {
+         :name => "Benvolio", 
+         :age => "17", 
+         :attitude => "Worried"}
+         {
+         :name => "Mercutio"
+         :age => "18", 
+         :attitude => "Hot-headed"}
+         ]
      }
      :capulet => {
-       :patriarch => {}, 
-       :matriarch => {}, 
-       :heroine => {}, 
-       :heroine_friends => [] #remember this is in a array because there are multiple variables being inserted here
+       :patriarch => {
+         :name => "Lord Capulet", 
+         :age => "50"
+       }, 
+       :matriarch => {
+         :name => "Lady Capulet",
+         :age => "51"
+       }, 
+       :heroine => {
+         :name => "Juliet", 
+         :age => "15",
+         :status => "Alive"
+       }, 
+       :heroine_friends => [{
+         :name => "Steven",
+         :age => "30",
+         :attitude => "Confused"}
+         {
+         :name => "Nurse", 
+         :age => "44", 
+         :attitude => "Worried"
+         }
+         ] #remember this is in a array because there are multiple variables being inserted here
      }
    }
 end
